5d47714b7d225b58fa30df06d8a34ba0f894b56d
ScottDeev <amehelijah99@gmail.com>
2023-06-28 19:33:42 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const todoRoutes = require('./routes/todos')
 const userRoutes = require('./routes/users')
+const cors = require('cors')
 
 // initialize express
 const app = express();
